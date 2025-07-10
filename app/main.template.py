bf5a83b93ad05007ab722c96f18a8d9ad4a5ed6a
Dhiraj Kumar <gdhiraj499@gmail.com>
2025-07-11 01:58:17 +0530

    feat: add main.template.py for dynamic version injection

---
+++ app/main.template.py
@@ -0,0 +1,19 @@
+from fastapi import FastAPI, Request
+from fastapi.templating import Jinja2Templates
+from fastapi.staticfiles import StaticFiles
+from fastapi.responses import HTMLResponse
+
+app = FastAPI()
+
+VERSION = "{{VERSION}}"  # <-- placeholder
+
+app.mount("/static", StaticFiles(directory="app/static"), name="static")
+
+templates = Jinja2Templates(directory="app/templates")
+
+@app.get("/", response_class=HTMLResponse)
+def read_root(request: Request):
+    return templates.TemplateResponse("index.html", {
+        "request": request,
+        "version": VERSION
+    })
